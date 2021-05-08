a0780e2323bc09eb9a63e17814f40aaba1f47a34
ioanh <ioan.xristov@abv.bg>
2021-05-08 22:13:56 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Galaxies_and_Planets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
